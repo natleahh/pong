39cd393b098cf990331349126412b52eefa00594
natleahh <71596982+natleahh@users.noreply.github.com>
2024-05-25 21:14:26 +0100

    added ball bounce logic

--- Ball.gd
+++ Ball.gd
@@ -9,7 +9,6 @@ var direction: int = 1
 func serve(start):
 	direction *= -1
 	heading = Vector2.UP.rotated(PI * randfn(0.5 - SERVE_VARIANCE, 0.5 + SERVE_VARIANCE)) * direction
-	print(heading)
 	global_position = start
 
 func move(delta):
--- pong.gd
+++ pong.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
 var window_size: Vector2i
+var centre: Vector2i
 
 func _ready():
 	window_size = get_viewport_rect().size
 
-	var centre = window_size / 2
+	centre = window_size / 2
 
 	for child in get_children():
 		child.position = centre
@@ -19,4 +20,7 @@ func _process(delta):
 		pallete.move(0, window_size.y, delta)
 	$Ball.move(delta)
 	
-	
+	if 0 > $Ball.position.y or $Ball.position.y > window_size.y:
+		$Ball.heading = $Ball.heading.bounce(Vector2i.UP)
+	if 0 > $Ball.position.x or $Ball.position.x > window_size.x:
+		$Ball.serve(centre)
